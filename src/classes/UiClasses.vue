51419355316ed593b1d0d20f3eec8134d12b7f28
Shemmy3 <shemmynyirenda3@gmail.com>
2022-08-02 20:23:51 +0200

    chnage vite dist folder

---
+++ src/classes/UiClasses.vue
@@ -0,0 +1,49 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+</script>
+
+<script lang="ts">
+import { FunctionalComponent } from "vue";
+
+//classes
+export class MenuItem {
+  private id: number;
+  private icon: FunctionalComponent;
+  private title: string;
+
+  /*
+    constructor() { }
+    */
+
+  constructor(id: number, icon: FunctionalComponent, title: string) {
+    this.id = id;
+    this.icon = icon;
+    this.title = title;
+  }
+
+  //setters
+  set Id(id: number) {
+    this.Id = id;
+  }
+  set Icon(icon: FunctionalComponent) {
+    this.icon = icon;
+  }
+  set Title(title: string) {
+    this.title = title;
+  }
+
+  //getters
+  get Id() {
+    return this.id;
+  }
+  get Icon() {
+    return this.icon;
+  }
+  get Title() {
+    return this.title;
+  }
+}
+</script>
